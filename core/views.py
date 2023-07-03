ec3c0f53809cfb064025dee4dec92386af1c65e5
Fahim Rayhan <fahimrayhan786@gmail.com>
2023-07-04 03:24:07 +0600

    Initial Commit

---
+++ core/views.py
@@ -0,0 +1,8 @@
+from django.shortcuts import render
+
+# Create your views here.
+def index(req):
+    return render(req, 'core/index.html')
+
+def contact(req):
+    return render(req,'core/contact.html')
