199de6e5cd3d88c6cb0b52f74d1513d87702bcf9
Fahim Rayhan <fahimrayhan786@gmail.com>
2023-07-08 05:29:59 +0600

    Register and SignIn option added

--- core/views.py
+++ core/views.py
@@ -1,8 +1,33 @@
-from django.shortcuts import render
+from django.shortcuts import redirect, render
+
+from item.models import Category, Item
+
+from .forms import SignupForm
 
 # Create your views here.
 def index(req):
-    return render(req, 'core/index.html')
+    
+    items = Item.objects.filter(is_sold=False)[0:3]
+    categoris = Category.objects.all()
+    
+    return render(req, 'core/index.html', {
+        'categories':categoris,
+        "items": items,
+    })
 
 def contact(req):
     return render(req,'core/contact.html')
+
+
+def signup(req):
+    if req.method == "POST":
+        form = SignupForm(req.POST)
+        
+        if form.is_valid():
+            form.save()
+            return redirect('/login/')
+    else:
+        form = SignupForm()
+    return render(req, 'core/signup.html', {
+        "form":form
+    })
--- item/views.py
+++ item/views.py
@@ -1,3 +1,35 @@
-from django.shortcuts import render
+from .forms import NewItem
+from .models import Item
+from django.shortcuts import redirect, render, get_object_or_404
+from django.contrib.auth.decorators import login_required
 
 # Create your views here.
+
+def details(req, pk):
+    item = get_object_or_404(Item, pk=pk)
+    
+    related_items = Item.objects.filter(category=item.category, is_sold=False).exclude(pk=pk)[0:3]
+    
+    return render(req, 'item/detail.html',{
+        'item':item,
+        'r_items':related_items,
+    })
+    
+@login_required
+def new(req):
+    if req.method == "POST":
+        form = NewItem(req.POST, req.FILES)
+        
+        if form.is_valid():
+            item = form.save(commit=False)
+            item.created_by = req.user
+            item.save
+            
+            return redirect('item:detail', pk=item.id)
+    else:
+        form = NewItem()
+    
+    return render(req, 'item/form.html',{
+        'form':form,
+        "title":'New Item',
+    })
